ece9906f36ddbda3d0be4ee794aebfe0479cd199
Stanislas P <57526019+quaxsze@users.noreply.github.com>
2021-10-04 14:05:54 +0200

    add missing config

--- app/config.py
+++ app/config.py
@@ -3,7 +3,11 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    ELASTICSEARCH_URL = os.environ.get('DOKKU_ELASTICSEARCH_RECHERCHE_URL') or 'http://localhost:9200'
+    ELASTICSEARCH_ADDR = os.environ.get(
+        'DOKKU_ELASTICSEARCH_POC_RECHERCHE_INDEX_PORT_9200_TCP_ADDR') or 'localhost'
+    ELASTICSEARCH_PORT = os.environ.get(
+        'DOKKU_ELASTICSEARCH_POC_RECHERCHE_INDEX_PORT_9200_TCP_PORT') or '9200'
+    ELASTICSEARCH_URL = f'http://{ELASTICSEARCH_ADDR}:{ELASTICSEARCH_PORT}'
     DATASET_CATALOG_URL = 'https://www.data.gouv.fr/fr/datasets/r/f868cca6-8da1-4369-a78d-47463f19a9a3'
     ORG_CATALOG_URL = 'https://www.data.gouv.fr/fr/datasets/r/b7bbfedc-2448-4135-a6c7-104548d396e7'
     DATASETS_PER_PAGE = 20
