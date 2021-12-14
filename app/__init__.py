71471f929852a9208a206ab59825ef539570de8e
quaxsze <plum.stanislas@gmail.com>
2021-12-14 17:08:44 +0100

    inital

--- app/__init__.py
+++ app/__init__.py
@@ -1,32 +1,29 @@
-import jinja_partials
-
-from flask import Flask
-from whitenoise import WhiteNoise
-from app.config import Config
+from fastapi import FastAPI
+from app.config import settings
 from app.containers import Container
-from app.presentation import seed, routes, api, markdown
+from app.presentation import seed, routes, api
+
+from app.presentation.api.api_v1 import api_router
 
 
-def create_app(config: object = Config) -> Flask:
+def create_app():
     container = Container()
     container.wire(modules=[api, routes, seed])
 
-    app: Flask = Flask(__name__)
-    app.container = container
-    app.config.from_object(config)
+    app = FastAPI(
+        title=settings.PROJECT_NAME, openapi_url=f"{settings.API_V1_STR}/openapi.json"
+        )
 
-    container.config.elasticsearch_url.from_value(app.config['ELASTICSEARCH_URL'])
-    container.config.search_synonyms.from_value(app.config['SEARCH_SYNONYMS'])
+    app.include_router(api_router, prefix=settings.API_V1_STR)
+
+    app.container = container
+
+    container.config.elasticsearch_url.from_value(settings.ELASTICSEARCH_URL)
+    container.config.search_synonyms.from_value(settings.SEARCH_SYNONYMS)
 
     # register the database command
     seed.init_app(app)
-    markdown.init_app(app)
 
-    app.register_blueprint(routes.bp)
     app.register_blueprint(api.bp)
-
-    app.wsgi_app = WhiteNoise(app.wsgi_app, root='app/static/')
-
-    jinja_partials.register_extensions(app)
 
     return app
--- app/config.py
+++ app/config.py
@@ -1,30 +1,27 @@
-import os
+import secrets
+from typing import List
+
+from pydantic import AnyHttpUrl, BaseSettings
 
 
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+class Settings(BaseSettings):
+    API_V1_STR: str = "/api/v1"
+    SECRET_KEY: str = secrets.token_urlsafe(32)
+    # 60 minutes * 24 hours * 8 days = 8 days
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8
+    SERVER_NAME: str
+    SERVER_HOST: AnyHttpUrl
 
-    ELASTICSEARCH_ADDR = os.environ.get(
-        'DOKKU_ELASTICSEARCH_POC_RECHERCHE_PORT_9200_TCP_ADDR') or 'localhost'
-    ELASTICSEARCH_PORT = os.environ.get(
-        'DOKKU_ELASTICSEARCH_POC_RECHERCHE_PORT_9200_TCP_PORT') or '9200'
-    ELASTICSEARCH_URL = f'http://{ELASTICSEARCH_ADDR}:{ELASTICSEARCH_PORT}'
+    PROJECT_NAME: str
 
-    DATASET_CATALOG_URL = 'https://www.data.gouv.fr/fr/datasets/r/f868cca6-8da1-4369-a78d-47463f19a9a3'
-    ORG_CATALOG_URL = 'https://www.data.gouv.fr/fr/datasets/r/b7bbfedc-2448-4135-a6c7-104548d396e7'
+    ELASTICSEARCH_ADDR: str = 'localhost'
+    ELASTICSEARCH_PORT: str = '9200'
+    ELASTICSEARCH_URL: AnyHttpUrl = f'http://{ELASTICSEARCH_ADDR}:{ELASTICSEARCH_PORT}'
 
-    # BASEROW_API_TOKEN = os.environ.get('POC_RECHERCHE_BASEROW_API_TOKEN') or 'secret'
-    # BASEROW_ORG_TYPOLOGY_URL = 'https://api.baserow.io/api/database/rows/table/18703/'
-    # BASEROW_ORG_BADGES_TO_BOOST_URL = 'https://api.baserow.io/api/database/rows/table/18698/'
+    DATASET_CATALOG_URL: AnyHttpUrl = 'https://www.data.gouv.fr/fr/datasets/r/f868cca6-8da1-4369-a78d-47463f19a9a3'
+    ORG_CATALOG_URL: AnyHttpUrl = 'https://www.data.gouv.fr/fr/datasets/r/b7bbfedc-2448-4135-a6c7-104548d396e7'
 
-    # SEARCH_DATASET_FIELDS = [
-    #     'title^6',
-    #     'description'
-    # ]
-    # SEARCH_DATASET_FEATURED_WEIGHT = 3
-    # SEARCH_DATASET_CERTIFIED_WEIGHT = 1.2
-
-    SEARCH_SYNONYMS = [
+    SEARCH_SYNONYMS: List = [
         "AMD, administrateur ministériel des données, AMDAC",
         "lolf, loi de finance",
         "waldec, RNA, répertoire national des associations",
@@ -32,74 +29,8 @@ class Config:
         "contour, découpage"
     ]
 
-    MD_ALLOWED_TAGS = [
-        'a',
-        'abbr',
-        'acronym',
-        'b',
-        'br',
-        'blockquote',
-        'code',
-        'dd',
-        'del',
-        'dl',
-        'dt',
-        'em',
-        'h1',
-        'h2',
-        'h3',
-        'h4',
-        'h5',
-        'h6',
-        'hr',
-        'i',
-        'img',
-        'li',
-        'ol',
-        'p',
-        'pre',
-        'small',
-        'span',
-        'strong',
-        'ul',
-        'sup',
-        'sub',
-        'table',
-        'td',
-        'th',
-        'tr',
-        'tbody',
-        'thead',
-        'tfooter',
-        # 'title',
-    ]
-
-    MD_ALLOWED_ATTRIBUTES = {
-        'a': ['href', 'title', 'rel', 'data-tooltip'],
-        'abbr': ['title'],
-        'acronym': ['title'],
-        'img': ['src', 'title']
-    }
-
-    MD_ALLOWED_STYLES = []
-
-    # Extracted from https://github.github.com/gfm/#disallowed-raw-html-extension-
-    MD_FILTERED_TAGS = [
-        'title',
-        'textarea',
-        'style',
-        'xmp',
-        'iframe',
-        'noembed',
-        'noframes',
-        'script',
-        'plaintext',
-    ]
-
-    MD_ALLOWED_PROTOCOLS = ['http', 'https', 'ftp', 'ftps']
+    class Config:
+        case_sensitive = True
 
 
-class Testing(Config):
-    TESTING = True
-    WTF_CSRF_ENABLED = False
-    ELASTICSEARCH_URL = 'http://localhost:9200'
+settings = Settings()
